bff2950e3de5ea560a32cb1fd308b6eb48ced87a
Badhan Sen <badhan.cse@gmail.com>
2023-07-07 03:39:26 +0600

    Pages apps with template

---
+++ 001Pages/pages/tests.py
@@ -0,0 +1,12 @@
+from django.test import SimpleTestCase
+
+
+class SimpeTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
