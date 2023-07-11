7e836c25da921cc1b99ab190a41af9b5d3089907
Badhan Sen <badhan.cse@gmail.com>
2023-07-11 22:49:28 +0600

    test cases added for users

--- 004Newspaper/users/views.py
+++ 004Newspaper/users/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 
 from .forms import CustomUserCreationForm
 
@@ -8,4 +8,8 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
 
