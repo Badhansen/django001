883906f7d18a110e92c638444c3f912a93502d07
Badhan Sen <badhan.cse@gmail.com>
2023-07-12 08:21:14 +0600

    bootstrap functionality add in front page

--- 004Newspaper/users/urls.py
+++ 004Newspaper/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import HomePageView, SignUpView
 
 
 urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
