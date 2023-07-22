8fcefce9ce7b680d4a55be10de22e0bc1b8d7da8
Badhan Sen <badhan.cse@gmail.com>
2023-07-22 16:23:18 +0600

    polished the article creation page and added the comment support for articles

--- 004Newspaper/articles/views.py
+++ 004Newspaper/articles/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView 
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy 
@@ -14,18 +15,35 @@ class ArticleDetailView(DetailView): # new
     template_name = 'article_detail.html'
 
 
-class ArticleUpdateView(UpdateView): # new
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): # new
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
 
+    login_url = 'login'
+
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 class ArticleDeleteView(DeleteView): 
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
 
-class ArticleCreateView(CreateView):
+    login_url = 'login'
+    
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
+    
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author',)
+    fields = ('title', 'body',)
+
+    login_url = 'login'
+
+    def form_valid(self, form): # new
+        form.instance.author = self.request.user
+        return super().form_valid(form)
